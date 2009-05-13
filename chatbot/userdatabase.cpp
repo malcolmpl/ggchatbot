b3e39cc46b4b852c07c940b6c9f26b65a21c362d
dariusz.mikulski <dariusz.mikulski@b801f502-efb6-11dd-99c0-75d572ba1ddd>
2009-05-13 14:42:20 +0000

    Fix adduser message

--- chatbot/userdatabase.cpp
+++ chatbot/userdatabase.cpp
@@ -102,7 +102,7 @@ UserInfoTOPtr UserDatabase::getUserInfo(uin_t uin) const
 
 void UserDatabase::addUser(const UserInfoTOPtr u)
 {
-    qDebug() << "Dodaje użytkownika do bazy.";
+    qDebug() << "Dodaje użytkownik" + u->getNick() + "do bazy.";
     
     foreach(UserInfoTOPtr user, m_usersList)
     {
